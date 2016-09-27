701b26b80f39973a5f48777031c2343c87902461
Luis Rene Onate <lonate12@gmail.com>
2016-09-27 13:18:46 -0400

    First function done.

---
+++ exercises.js
@@ -0,0 +1,102 @@
+/**
+ *
+ */
+
+// ---------------------
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
+// ---------------------
+
+function max(num1, num2){
+    "use strict";
+    if(num1 > num2){
+      return num1;
+    }else if (num1 < num2){
+      return num2;
+    }else {
+      var answer = 'They are equal';
+      return answer;
+    }
+}
+
+  var largerNumber = max(100, 500);
+  console.assert(largerNumber == 500);
+  console.assert(max(-10, -5) == -5);
+  console.assert(max(5, 5) == 'They are equal');
+  console.log(largerNumber);
+
+// ---------------------
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// ---------------------
+
+function maxOfThree(){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// ---------------------
+
+function isVowel(char){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+// ---------------------
+
+function rovarspraket(phrase){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
+// ---------------------
+
+function sum(){
+    "use strict";
+    //...
+}
+
+function multiply(){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+// ---------------------
+
+function reverse(){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
+// ---------------------
+
+function findLongestWord(words){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
+// ---------------------
+
+function filterLongWords(words, i){
+    "use strict";
+    //...
+}
+
+// ---------------------
+// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
+// ---------------------
+
+function charFreq(string){
+    "use strict";
+    //...
+}
